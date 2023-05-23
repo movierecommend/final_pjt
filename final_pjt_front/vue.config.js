4c551813ad81bd381a06455c7f200dc07bb14b1c
sangmihwang <01071184366a@gmail.com>
2023-05-23 11:57:01 +0900

    230523 | Sangmi | 게시판 생성확인

--- final_pjt_front/vue.config.js
+++ final_pjt_front/vue.config.js
@@ -9,8 +9,8 @@ module.exports = defineConfig({
 module.exports = {
   devServer: {
     proxy: {
-      '^/api': {
-        target: 'https://movie.daum.net', // 프록시 대상 URL
+      '/api': {
+        target: 'http://localhost:8000/api', // 프록시 대상 URL
         changeOrigin: true,
         pathRewrite: {
           '^/api': '', // 요청 URL에서 '/api'를 제거
@@ -18,4 +18,5 @@ module.exports = {
       },
     },
   },
+  outputDir: '../backend/public',
 };
