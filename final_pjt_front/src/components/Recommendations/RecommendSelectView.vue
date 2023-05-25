0441a9114988724cb82f99a596d5cce8ff39a722
legrandreve <legrandreve@naver.com>
2023-05-26 06:08:21 +0900

    230526 | fun 페이지 flexbox 배치 및 팝업

--- final_pjt_front/src/components/Recommendations/RecommendSelectView.vue
+++ final_pjt_front/src/components/Recommendations/RecommendSelectView.vue
@@ -4,20 +4,26 @@
       <video ref="videoPlayer" src="/Recommendation.mp4" controls autoplay @ended="playbackEnded"></video>
     </div>
     <div v-else>
-    <div class="d-container">
-      <div v-if="!isAnswered" class="question-container">
-        <h1>{{ currentQuestion.question }}</h1>
-          <button v-for="(choice, i) in currentQuestion.choices" :key="i" @click="selectChoice(choice, i)" class="button-option">
+      <div class="d-container">
+        <div v-if="!isAnswered" class="question-container">
+          <h1>{{ currentQuestion.question }}</h1>
+        </div>
+        <div class="answer-container" v-else>
+          <button v-for="(choice, i) in currentQuestion.choices" :key="i" @click="selectChoice(choice, i)" :class="{ 'selected': selectedChoice === choice }" class="button-option">
             {{ choice }}
+            <!-- <span v-if="selectedChoice === choice" class="selected-choice">{{ selectedChoice }}</span> -->
           </button>
-      </div>
-        <div v-else class="answer-container">
-          <h2>{{ selectedChoice }}</h2>
+          
           <button v-if="currentQuestionIndex < questions.length - 1" @click="nextQuestion" class="button-next">Next</button>
           <button v-else @click="showRandomMovie" class="button-findout">Find out!</button>
         </div>
+        <div class="button-container">
+          <button v-for="(choice, i) in currentQuestion.choices" :key="i" @click="selectChoice(choice, i)" class="button-option">
+            {{ choice }}
+          </button>
+        </div>
       </div>
-    <div v-if="showPopup" class="popup-overlay" v-on:click="closePopup">
+      <div v-if="showPopup" class="popup-overlay" v-on:click="closePopup">
         <div class="popup-content">
           <h2>{{ randomMovie.fields.title }}</h2>
           <img :src="randomMovie.fields.poster_path" alt="Movie Poster">
@@ -42,7 +48,7 @@ export default {
       questions: [
         {
           question: '외나무다리에서 만난 내 부모님의 원수의 딸(아들), 절세미녀(남)인 그녀(그)에게 나는?',
-          choices: ['사랑에 빠진다', '부모님의 복수를 한다'],
+          choices: ['사랑에 빠진다', '복수를 한다'],
           genres: [23, 2, 8, 6, 27, 17],
           results: ['romance 장르 선택!', 'crime 장르 선택!']
         },
@@ -166,65 +172,83 @@ export default {
 
 <style>
 @font-face {
-  font-family: 'jua';
-  src:url('../../../public/fonts/BMJUA_ttf.ttf')
+  font-family: 'dh';
+  src:url('../../../public/fonts/BMDOHYEON_ttf.ttf')
 }
-
-.question-container {
+.d-container {
   width: 80%;
-  height: 150px;
   margin: 0 auto;
   padding: 20px;
   background-color: rgb(219, 214, 214);
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-family: 'jua';
-}
-.answer-container {
-  width: 600px;
-  height: 50%;
-  margin: 200px auto;
-  padding: 20px;
-  background-color: rgb(219, 214, 214);
+  font-family: 'dh';
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.question-container {
+  width: 100%;
+  height: 150px;
+  margin-bottom: 20px;
+  background-color: rgb(219, 214, 214);
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-align: center;
-  font-family: 'jua';
+}
+.answer-container {
+  width: 100%;
+  height: 150px;
+  background-color: rgb(219, 214, 214);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 h3 {
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 10px;
-  font-family: 'jua';
+  font-family: 'dh';
 }
 video {
-  width: 1500px;
+  width: 100%;
 }
 .container h1 {
   font-size: 24px;
   font-weight: bold;
   margin-bottom: 20px;
 
-  font-family: 'jua';
+  font-family: 'dh';
+}
+.button-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+  align-items: center;
 }
 
 .button-option {
   padding: 8px 16px;
   margin: 10px;
-  font-size: 16px;
-  background-color: #e06a6a;
+  font-size: 50px;
+  background-color: #df596b;
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  width: 500px;
-  height: 500px;
   
-  font-family: 'jua';
+  font-family: 'dh';
+}
+.button-option.selected {
+  background-color: #7a7070;
+}
+
+.selected-choice {
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 10px;
+  font-family: 'dh';
 }
 .button-next {
   padding: 8px 16px;
@@ -235,7 +259,7 @@ video {
   border-radius: 4px;
   cursor: pointer;
   
-  font-family: 'jua';
+  font-family: 'dh';
 }
 .button-findout {
   padding: 8px 16px;
@@ -246,7 +270,7 @@ video {
   border-radius: 4px;
   cursor: pointer;
   
-  font-family: 'jua';
+  font-family: 'dh';
 }
 
 button:hover {
@@ -258,7 +282,7 @@ h4 {
   font-weight: bold;
   margin-top: 20px;
   
-  font-family: 'jua';
+  font-family: 'dh';
 }
 .popup-overlay {
   position: fixed;
@@ -272,7 +296,7 @@ h4 {
   background-color: rgba(0, 0, 0, 0.5);
   cursor: pointer;
   
-  font-family: 'jua';
+  font-family: 'dh';
 }
 
 .popup-content {
@@ -280,7 +304,7 @@ h4 {
   padding: 20px;
   text-align: center;
   
-  font-family: 'jua';
+  font-family: 'dh';
 }
 
 .popup-content img {
--- final_pjt_front/src/views/FunView.vue
+++ final_pjt_front/src/views/FunView.vue
@@ -2,56 +2,60 @@
   <div>
     <br>
     <h2>내가 영화 캐릭터라면?</h2>
-    <div v-if="!showPopup" class="container" style="flex-wrap: nowrap;">
-      <div v-for="option in optionsEI" :key="option.id">
-        <label :for="option.id" @click="selectOptionEI(option)">
-          <img :src="option.image" :class="{'checked': selectedOptionEI === option}" />
+      <div class="d-container">
+        <div v-if="!showPopup" class="d-flex" style="justify-content: center;">
+          <div>
+            <div v-for="option in optionsEI" :key="option.id">
+              <label :for="option.id" @click="selectOptionEI(option)">
+                <img :src="option.image" :class="{'checked': selectedOptionEI === option}" />
 
-        <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueEI" hidden />
-      </label>
-    </div>
-    <div v-for="option in optionsNS" :key="option.id">
-        <label :for="option.id" @click="selectOptionNS(option)">
-          <img :src="option.image" :class="{'checked': selectedOptionNS === option}" />
-        <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueNS" hidden />
-      </label>
-    </div>
-    <div v-for="option in optionsTF" :key="option.id">
-        <label :for="option.id" @click="selectOptionTF(option)">
-          <img :src="option.image" :class="{'checked': selectedOptionTF === option}" />
-        <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueTF" hidden />
-      </label>
-    </div>
-    <div v-for="option in optionsPJ" :key="option.id">
-        <label :for="option.id" @click="selectOptionPJ(option)">
-          <img style="width: 204px;" :src="option.image" :class="{'checked': selectedOptionPJ === option}" />
-        <input type="radio" :id="option.id" :value="option.value" v-model="selectedValuePJ" hidden />
-      </label>
+              <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueEI" hidden />
+            </label>
+          </div>
+        </div>
+        <div>
+          <div style="flex-wrap:nowrap;" v-for="option in optionsNS" :key="option.id">
+              <label :for="option.id" @click="selectOptionNS(option)">
+                <img :src="option.image" :class="{'checked': selectedOptionNS === option}" />
+              <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueNS" hidden />
+            </label>
+          </div>
+        </div>
+        <div>
+          <div style="flex-wrap:nowrap;" v-for="option in optionsTF" :key="option.id">
+              <label :for="option.id" @click="selectOptionTF(option)">
+                <img :src="option.image" :class="{'checked': selectedOptionTF === option}" />
+              <input type="radio" :id="option.id" :value="option.value" v-model="selectedValueTF" hidden />
+            </label>
+          </div>
+        </div>
+        <div>
+          <div style="flex-wrap:nowrap;" v-for="option in optionsPJ" :key="option.id">
+              <label :for="option.id" @click="selectOptionPJ(option)">
+                <img :src="option.image" :class="{'checked': selectedOptionPJ === option}"/>
+              <input type="radio" :id="option.id" :value="option.value" v-model="selectedValuePJ" hidden />
+            </label>
+          </div>
+        </div>
+      </div>
+    <div>
+      <button v-if="!showPopup" @click="findYourMBTI()" style="margin-top: 20px;">확인</button>
     </div>
   </div>
-
     <div>
-      
-<<<<<<< HEAD
-      <button @click="findYourMBTI()">확인</button>
-      <div>
-        <div v-for="mbti in mbti_jsonData" :key="mbti.id">
-          <div v-if="mbti.fields.MBTI === yourMBTI">
-            {{ mbti.fields.character }}
-            <img class="result" :src="mbti.fields.image" alt="MBTI 이미지">
-=======
-      <button v-if="!showPopup" @click="findYourMBTI()">확인</button>
+      <h1 v-if="showPopup">당신은 "{{ yourMBTI }}"</h1>
         <div> 
           <div v-if="showPopup" class="popup-overlay" @click="closePopup">
             <div v-for="mbti in mbti_jsonData" :key="mbti.id">
               <div v-if="mbti.fields.MBTI === yourMBTI">
                 <div id="actorcontainer">
-                  <p >{{ mbti.fields.character }}</p>
-                  <img class="result" :src="mbti.fields.image" alt="MBTI 이미지">
+                  <h3>{{ mbti.fields.character }}</h3>
+                  <img v-if="!showtitle" class="result" :src="mbti.fields.image" alt="MBTI 이미지">
+                  <div class="result" v-else>{{ mbti.fields.title }}</div>
+                  <div class="result"><h4>{{ mbti.fields.title }}</h4></div>
                 </div>
               </div>
             </div>
->>>>>>> 9c8e4967a4cd6cf2e11059f7facac9c2cc9c80c4
           </div>
         </div> 
     </div>
@@ -90,7 +94,8 @@ export default {
         { id: 7, image: './P.png', value: 'P' },
         { id: 8, image: './J.png', value: 'J' },
       ],
-      showPopup: false,
+      showPopup: true,
+      showtitle: false,
     }
   },
   methods: {
@@ -132,6 +137,9 @@ export default {
     findYourMBTI() {
       this.yourMBTI = this.selectedValueEI + this.selectedValueNS + this.selectedValueTF + this.selectedValuePJ      
       this.showPopup = !this.showPopup
+    },
+    showtitlefunc() {
+      this.showtitle = !this.showtitle
     }
   },
   mounted() {
@@ -142,6 +150,11 @@ export default {
 
 
 <style>
+@font-face {
+  font-family: 'dh';
+  src:url('../../public/fonts/BMDOHYEON_ttf.ttf')
+}
+
 img {
   width: 150px;
   height: 150px;
@@ -158,11 +171,25 @@ img {
 
 }
 .checked {
-  border: 4px solid rgb(255, 242, 65);
+  border: 4px solid #e6724f;
   box-sizing: border-box;
   width: 150px;
   height: 150px;
 }
+
+button {
+    position: absolute;
+    top:50%;
+    background-color:#e6724f;
+    color: white;
+    border:none; 
+    border-radius:10px; 
+    padding:15px;
+    min-height:30px; 
+    min-width: 120px;
+    font-family: 'dh';
+  }
+
 .container {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -182,7 +209,7 @@ img {
   top: 35vh;
   left: 0;
   width: 100%;
-  height: 30%;
+  height: 40%;
   display: flex;
   justify-content: center;
   align-items: center;
