0b1128eb238fe9964a06cf79eda9807da3443ceb
이연주 <legrandreve@naver.com>
2023-05-26 09:14:53 +0900

    제출

--- final_pjt_front/src/views/ArticleListView.vue
+++ final_pjt_front/src/views/ArticleListView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="d-container">
     <h1>Article Page</h1>
+
     <router-link :to="{ name: 'create' }" v-if="isLoggedIn">[CREATE]</router-link>
     <ul>
       <li v-for="article in articles" :key="article.pk">
@@ -18,6 +19,7 @@
 
         <!-- Info -->
         ({{ article.comment_count }}) | +{{ article.like_count }}
+      <hr>
       </li>
     </ul>
 
@@ -43,5 +45,15 @@ export default {
 </script>
 
 <style>
-
+.d-container {
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: rgb(219, 214, 214);
+  font-family: 'dh';
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
